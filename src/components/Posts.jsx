54900e2422925670e5ed0e0e1d4f4eae188901b3
emeliesv <emeliesv@live.se>
2024-05-20 16:00:55 +0200

    sorted posts and did more styling

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -17,35 +17,37 @@ const Posts = () => {
 
   return (
     <div>
-      {posts.map((post) => {
-        return (
-          <div
-            className="border-solid border-purple-950 border m-5 p-5 bg-purple-50"
-            key={post.id}
-          >
-            <h3 className="text-purple-800 font-medium text-xl">
-              {post.title}
-            </h3>
-            <p className="italic font-light">{post.author}</p>
-            <p className="mb-5">{post.bodyText}</p>
-            {currentUser.email === post.author && (
-              <div className="flex my-5">
-                <Link className="buttonstyle" to={`/edit/${post.id}`}>
-                  Edit
-                </Link>
-                <button onClick={() => handleDelete(post.id)}>Delete</button>
-              </div>
-            )}
-            <Link
-              to={`/${post.id}`}
-              className="font-bold text-purple-900 underline hover:text-purple-700"
+      {posts
+        .sort((a, b) => b.id - a.id)
+        .map((post) => {
+          return (
+            <div
+              className="border-solid border-purple-950 border m-5 p-5 bg-purple-50"
+              key={post.id}
             >
-              Read more
-            </Link>
-            <Comments postID={post.id} />
-          </div>
-        );
-      })}
+              <h3 className="text-purple-800 font-medium text-xl">
+                {post.title}
+              </h3>
+              <p className="italic font-light">{post.author}</p>
+              <p className="mb-5">{post.bodyText}</p>
+              {currentUser.email === post.author && (
+                <div className="flex my-5">
+                  <Link className="buttonstyle" to={`/edit/${post.id}`}>
+                    Edit
+                  </Link>
+                  <button onClick={() => handleDelete(post.id)}>Delete</button>
+                </div>
+              )}
+              <Link
+                to={`/${post.id}`}
+                className="font-bold text-purple-900 underline hover:text-purple-700"
+              >
+                Read more
+              </Link>
+              <Comments postID={post.id} />
+            </div>
+          );
+        })}
     </div>
   );
 };
