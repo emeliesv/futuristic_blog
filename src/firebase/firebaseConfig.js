6debd8fa669936d8d7fab5ed87ad2b5f6955f8a6
emeliesv <emeliesv@live.se>
2024-05-22 14:58:58 +0200

    changed secrets

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,18 +2,17 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { firebaseSecret } from "../secret";
 
 // Your web app's Firebase configuration
-const firebaseConfig = firebaseSecret; /* {
-  apiKey: "AIzaSyC2M_wtbbpj8e34xNdTVJK2QfD7EnKUKPM",
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "authentication-3be54.firebaseapp.com",
   projectId: "authentication-3be54",
   storageBucket: "authentication-3be54.appspot.com",
   messagingSenderId: "552757413459",
   appId: "1:552757413459:web:36a6580448af0039e468a6",
   measurementId: "G-LDGXNW01VX",
-} */
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
